03ffcca5e6202153d999d9e7945ac2d196da5433
code-unique <uniqueadhikari2534@gmail.com>
2025-07-29 22:23:15 +0545

    done1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import { Star, Video, Users, Brush } from "lucide-react";
-import Image from "next/image";
+
 
 export default function Home() {
   const { data: session } = useSession();
@@ -139,7 +139,7 @@ export default function Home() {
       transition={{ duration: 0.8 }}
       className="w-40 h-40 md:w-56 md:h-56 overflow-hidden rounded-full shadow-xl mx-auto md:mx-0"
     >
-      <Image src="/founder.jpg" alt="Merina Giri" width={500} height={600} priority />
+      
     </motion.div>
 
     <motion.div
